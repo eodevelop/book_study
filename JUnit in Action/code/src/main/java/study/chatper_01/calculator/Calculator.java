abe2fc1bd551a37a5bcf72adb6b05e9d58e21257
eodevelop <eodevelop@naver.com>
2024-10-16 13:31:56 +0900

    test

--- JUnit in Action/code/src/main/java/study/chatper_01/calculator/Calculator.java
+++ JUnit in Action/code/src/main/java/study/chatper_01/calculator/Calculator.java
@@ -1,7 +1,10 @@
 package study.chatper_01.calculator;
 
 public class Calculator {
+
     public double add(double number1, double number2) {
         return number1 + number2;
+
+        // test
     }
 }
