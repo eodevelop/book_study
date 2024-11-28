c4da83c7fd648dda72c1541ebb38f9372e0bf0a7
eodevelop <eodevelop@naver.com>
2024-11-28 11:42:03 +0900

    1장 마무리

--- JUnit in Action/code/src/test/java/study/chatper_01/calculator/CalculatorTest.java
+++ JUnit in Action/code/src/test/java/study/chatper_01/calculator/CalculatorTest.java
@@ -1,15 +1,15 @@
 package study.chatper_01.calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class CalculatorTest {
+class CalculatorTest { //  1)
 
-    @Test
+    @Test // 2)
     public void testAdd() {
-        Calculator calculator = new Calculator();
-        double result = calculator.add(10, 20);
-        assertEquals(30, result, 0); // delta는 부동 소수점의 결과가 오류가 있을경우 자주 쓰인다.
+        Calculator calculator = new Calculator(); // 3)
+        double result = calculator.add(10, 20); // 4)
+        assertEquals(30, result, 0); // 5) delta는 부동 소수점의 결과가 오류가 있을경우 자주 쓰인다.
     }
 }
